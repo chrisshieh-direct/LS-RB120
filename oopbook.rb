2a2ed578b1f686d0207bba93112b68f02c15bb59
Chris Shieh <dev@chrisgrace.com>
2022-11-11 22:53:58 -0800

    done with book

--- oopbook.rb
+++ oopbook.rb
@@ -1,38 +1,80 @@
-class MyCar
-  attr_accessor :color
-  attr_reader :year, :model
+class Vehicle
+  attr_accessor :color, :current_speed, :engine_on, :name
+  attr_reader :year, :make, :units
 
-  def initialize(year, color, model)
+  @@units = 0
+
+  def initialize(year, color, make)
+    @@units += 1
     @year = year
     @color = color
-    @model = model
+    @make = make
     @current_speed = 0
+    @engine_on = false
   end
 
-  def speed_up
-    current_speed += 10
+  def speed_up(increase)
+    @current_speed += increase
   end
 
-  def brake
-    current_speed = 0
+  def brake(decrease)
+    @current_speed -= decrease
   end
 
-  def shut_off
+  def shut_off(bool)
+    @current_speed = 0
+    @engine_on = false
   end
 
-  def info
-    puts "This is a #{color} #{year} #{model}. Noice."
+  def to_s
+    puts "The year is #{year} and the color is #{color}."
   end
 
-  def spray_paint(new_color)
-    self.color = new_color
+  def spray_paint(str)
+    @color = str
+  end
+
+  def self.gas(x, y)
+    puts "#{y / x} miles per gallon of gas"
+  end
+
+  def self.howmany
+    puts "#{@@units} okay"
   end
 end
 
-buick = MyCar.new(2022, "Black", "Skylark")
+module Cargo
+  def LIIIIIFFFFFFT
+    puts "The cargo door is open."
+  end
+end
 
-buick.info
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+end
 
-buick.spray_paint("Turqoise")
+class MyTruck < Vehicle
+  include Cargo
+  NUMBER_OF_DOORS = 2
+end
 
-buick.info
+buick = MyCar.new(1973, "Black", "Skyhawk")
+buick2 = MyCar.new(1973, "Black", "Skyhawk")
+buick3 = MyCar.new(1973, "Black", "Skyhawk")
+buick4 = MyCar.new(1973, "Black", "Skyhawk")
+
+buick.spray_paint("REDDDD")
+
+buick.to_s
+
+
+buick.name
+
+MyCar.howmany
+
+hoss = MyTruck.new(1953,"Red", "F-150")
+
+hoss.LIIIIIFFFFFFT
+
+puts MyTruck.ancestors
+puts Vehicle.ancestors
